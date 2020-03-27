e95e3ac6cce08e19ff63a02747cdb3a68e27261e
Alexander <alexy-lex2013@yandex.ru>
2020-03-27 14:09:29 +0300

    common password reset validation

--- authapp/models.py
+++ authapp/models.py
@@ -7,15 +7,14 @@ from authapp.managers import CustomUserManager
 class CustomUser(AbstractUser):
     username = models.CharField(max_length=60, unique=True, null=True)
     email = models.EmailField(_('email address'), unique=True)
+    spouse_name = models.CharField(blank=True, max_length=100)
+    date_of_birth = models.DateField(blank=True, null=True)
 
     USERNAME_FIELD = 'username'
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = ['email', 'password']
 
     objects = CustomUserManager()
-
-    spouse_name = models.CharField(blank=True, max_length=100)
-    date_of_birth = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return self.username
--- authapp/viewsets.py
+++ authapp/viewsets.py
@@ -1,10 +1,10 @@
-from rest_framework import viewsets, permissions, status
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 
 from authapp.models import CustomUser
 from authapp.serializers import CustomUserSerializer
+from CollectionDriveBackEnd.permissions import IsOwnerOfUserOrReadOnly
 
 
 class CustomUserViewSet(viewsets.ModelViewSet):
@@ -22,3 +22,15 @@ class CustomUserViewSet(viewsets.ModelViewSet):
             return Response(data={'Error': 'The Password must be set'}, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'], permission_classes=[IsOwnerOfUserOrReadOnly])
+    def set_password(self, request, pk):
+        user = self.get_object()
+        if request.data['password1'] == request.data['password2'] and len(request.data['password1']) >= 8:
+            user.set_password(request.data['password1'])
+            return Response(data='Password was successfully set', status=status.HTTP_202_ACCEPTED)
+        elif len(request.data['password1']) < 8:
+            return Response(data='Password must have at least 8 symbols', status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(data='Passwords do not match', status=status.HTTP_400_BAD_REQUEST)
+
